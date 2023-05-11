21814b2750154c852dd871cc84e0f8126843ecf7
Ekaterina2023 <katerina_kochetkova15@mail.ru>
2023-05-11 17:05:51 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -39,9 +39,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "dd3c1ef027c41784a5c06f57ecaeb879";
 
-let apiKey = "dd3c1ef027c41784a5c06f57ecaeb879";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
